61a64dd49768ad8c5ff1e62cba1eef1016f5021e
Lochoa11 <linochoa11690@gmail.com>
2025-01-08 18:42:56 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Lin Ochoa on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
